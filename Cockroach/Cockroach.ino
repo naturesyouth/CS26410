72b1c7bbe7bddbc60e88abe5bfbf89ecffde0dc5
William Hogg <wih6@aber.ac.uk>
2016-03-14 14:04:26 +0000

    stuff

--- Cockroach/Cockroach.ino
+++ Cockroach/Cockroach.ino
@@ -60,7 +60,7 @@ Description:
 #define Function_2	56
 #define Function_3      57
 
-//object instantition 
+//object instantition
 Servo Driveleft;
 Servo Driveright;
 Servo Turret;
@@ -76,8 +76,7 @@ struct w {
 };
 
 struct boxcar {
-  int vals[3];
-  int average;
+  int vals[NoAverageDataSets];
   int index;
   int delta;
 };
@@ -88,194 +87,199 @@ struct motors {
 };
 
 //global variables
-int boxcar[MovingAverageWidth*NoAverageDataSets];
+int boxcar[MovingAverageWidth * NoAverageDataSets];
 int boxcar_index[NoAverageDataSets];
 
-int priority =0;
-int driveVal =0;
+int  ldrPins[] = {ldrPin1, ldrPin2, ldrPin3};
+int calibrateHigh[] = {0, 0, 0}; //these arrays store the high and low values for sensor calibration
+int calibrateLow[] = {0, 0, 0};
 
-struct boxcar pings[1];
+int priority = 0;
+int driveVal = 0;
+
+struct boxcar pings[4];
 struct w  walls[2];
 
 
 void setup() {
-  
+
   Driveleft.attach(Dleft);
   Driveleft.write(DleftStop);
   Driveright.attach(Dright);
   Driveright.write(DrightStop);
- // Turret.attach(turret);
- // Turret.write(TurretCenter);
-  
+  // Turret.attach(turret);
+  // Turret.write(TurretCenter);
+
   pinMode(LDRLeft, INPUT);
   pinMode(LDRCenter, INPUT);
   pinMode(LDRRight, INPUT);
   pinMode(LDRTop, INPUT);
-  pinMode(PB1,INPUT);
-  
+  pinMode(PB1, INPUT);
+
   pinMode(SharpIrEnable, OUTPUT);
   pinMode(LEDRed, OUTPUT);
-  
-  Serial.begin(9600);  
-  
+
+  Serial.begin(9600);
+
   //ini the moving average array
-  for(int i = 0; i < MovingAverageWidth; i++){      
-      boxcar_addValue(analogRead(LDRTop),BrightnessTop);
-      delay(2);
-  }    
+  for (int i = 0; i < MovingAverageWidth; i++) {
+    boxcar_addValue(analogRead(LDRTop), BrightnessTop);
+    delay(2);
+  }
   //ini the moving average for sonars
-  for(int p =1;p<=4;p++){
-      for(int i = 0; i < MovingAverageWidth; i++){   
-          readSonar(p);
-          delay(2);
-      }
-  }  
-}
-
-
-void loop(){
-    driveVal =0;
-    priority =0;
-    //Serial.println("BOOP LOOP");
-    boxcar_addValue(analogRead(LDRTop),BrightnessTop);
-    readSonar(1);
-    readSonar(2);
-    readSonar(3);
-    readSonar(4);
-    //Serial.print("LDRTop = ");
-    //Serial.println(boxcarGetAverage(BrightnessTop));
-    //Serial.print("sonar 3 says ");
-    //Serial.println(boxcarGetAverage(3));
-    //Serial.println(sonar1.ping());
-    //Serial.println("avoid");
-    avoider();
-    //Serial.println("sense");
-    sensitivity();
-    //Serial.println("follow");
-    //followWall();
-    
-    Serial.print(priority);
-    Serial.println(driveVal);
-    motor_basic(driveVal);
-    delay(500);
-    motor_basic(1);
-    delay(500);
-}
-
-
-void followWall(){
-    int range = pings[3].average;
-    if( range <= 7){
-        motor_queue(2,3);
-    }else if(range >= 7){
-        motor_queue(2,2);
+  for (int p = 1; p <= 4; p++) {
+    for (int i = 0; i < MovingAverageWidth; i++) {
+      readSonar(p);
+      delay(2);
     }
+  }
 }
 
 
-void avoider(){
-    //int leftRange = boxcarGetAverage(2);
-    //int rightRange = boxcarGetAverage(3);
-   if(pings[1].average <= 5 || pings[2].average <= 5 || pings[0].average <= 5 || pings[3].average <= 5){
-      motor_queue(3,3);
-       //delay(500);
-   }
+void loop() {
+  driveVal = 0;
+  priority = 0;
+  //Serial.println("BOOP LOOP");
+  boxcar_addValue(analogRead(LDRTop), BrightnessTop);
+  readSonar(1);
+  readSonar(2);
+  readSonar(3);
+  readSonar(4);
+  //Serial.print("LDRTop = ");
+  //Serial.println(boxcarGetAverage(BrightnessTop));
+  //Serial.print("sonar 3 says ");
+  //Serial.println(boxcarGetAverage(3));
+  //Serial.println(sonar1.ping());
+  //Serial.println("avoid");
+  avoider();
+  //Serial.println("sense");
+  sensitivity();
+  //Serial.println("follow");
+  //followWall();
+
+  Serial.print(priority);
+  Serial.println(driveVal);
+  motor_basic(driveVal);
+  delay(500);
+  motor_basic(1);
+  delay(500);
 }
 
 
-void sensitivity(){
-    int brightness = boxcarGetAverage(BrightnessTop);
-    if ( brightness < Dark){
-        motor_queue(4,0);
-    }else if(brightness > Light){
-        //motor_basic(2);
-    }else{
-        motor_queue(0,1);
-    }    
+void followWall() {
+  int range = (boxcarGetAverage(&pings[3]));
+  if ( range <= 7) {
+    motor_queue(2, 3);
+  } else if (range >= 7) {
+    motor_queue(2, 2);
+  }
 }
 
 
-void readSonar(int pingNo){
-    int range = 0;
-    switch (pingNo) {
-        case 1:
-            pings[0] = sonar1.ping_cm();
-             
-            break;
-        case 2:
-            pings[1] = sonar2.ping_cm();
-            break;
-        case 3:
-            pings[2] = sonar3.ping_cm();
-            break;
-        case 4:
-            pings[3] = sonar4.ping_cm();
-            break;
-    } 
-      //Serial.println(pings[1]);
-      //Serial.println(pings[3]+" " + pings[2]);
-    //boxcar_addValue(range,pingNo);
+void avoider() {
+  //int leftRange = boxcarGetAverage(2);
+  //int rightRange = boxcarGetAverage(3);
+  if (boxcarGetAverage(&pings[0]) <= 5 || boxcarGetAverage(&pings[1]) <= 5 || boxcarGetAverage(&pings[2]) <= 5 || boxcarGetAverage(&pings[3]) <= 5) {
+    motor_queue(3, 3);
+    //delay(500);
+  }
+}
+
+
+void sensitivity() {
+  int brightness = boxcarGetAverage(BrightnessTop);
+  if ( brightness < Dark) {
+    motor_queue(4, 0);
+  } else if (brightness > Light) {
+    //motor_basic(2);
+  } else {
+    motor_queue(0, 1);
+  }
+}
+
+
+void readSonar(int pingNo) {
+  int range = 0;
+  switch (pingNo) {
+    case 1:
+
+      boxcar_addValue(sonar1.ping_cm(), &pings[0]);
+
+      break;
+    case 2:
+      boxcar_addValue(sonar2.ping_cm(), &pings[1]);
+      break;
+    case 3:
+      boxcar_addValue(sonar3.ping_cm(), &pings[2]);
+      break;
+    case 4:
+      boxcar_addValue(sonar4.ping_cm(), &pings[3]);
+      break;
+  }
+  //Serial.println(pings[1]);
+  //Serial.println(pings[3]+" " + pings[2]);
+  //boxcar_addValue(range,pingNo);
 }
 
 
 void remote(int val) {
   switch (val) {
-      case Forward:
-          //move forward
-          motor_basic(1);
-          break;
-      case Backward:
-          //move backwards
-          motor_basic(1);
-          break;
-      case Turret_Left:
-          //move turret left
-          break;
-      case Turret_Right:
-          //move Turret right
-          break;
-      case Pivot_Left:
-          //pivot robot left
-          motor_basic(2);
-          break;
-      case Pivot_Right:
-          //pivot robot right
-          motor_basic(3);
-          break;
-      case Stop:
-          //stop robot
-          motor_basic(0);
-          break;
-      case Function_1:
-          //implement function 1
-          break;
-      case Function_2:
-          //implement function 2
-          break;
-      case Function_3:
-          //implement function 2
-          break;
-      default:
-          //stuff perhaps
-          break;
+    case Forward:
+      //move forward
+      motor_basic(1);
+      break;
+    case Backward:
+      //move backwards
+      motor_basic(1);
+      break;
+    case Turret_Left:
+      //move turret left
+      break;
+    case Turret_Right:
+      //move Turret right
+      break;
+    case Pivot_Left:
+      //pivot robot left
+      motor_basic(2);
+      break;
+    case Pivot_Right:
+      //pivot robot right
+      motor_basic(3);
+      break;
+    case Stop:
+      //stop robot
+      motor_basic(0);
+      break;
+    case Function_1:
+      //implement function 1
+      break;
+    case Function_2:
+      //implement function 2
+      break;
+    case Function_3:
+      //implement function 2
+      break;
+    default:
+      //stuff perhaps
+      break;
   }
 }
 
-void motor_queue(int pri, int mot){
-    if(pri > priority){
-        priority = pri;
-        driveVal = mot;
-    }
+void motor_queue(int pri, int mot) {
+  if (pri > priority) {
+    priority = pri;
+    driveVal = mot;
+  }
 }
 
-   
+
 void motor_basic(int dir) {
   switch (dir) {
     case 0:
       //Stop motors
       Driveleft.write(DleftStop );
       Driveright.write(DrightStop);
-      break;    
+      break;
     case 1:
       //Drive forwards
       Driveleft.write(DleftStop + Speed);
@@ -299,28 +303,27 @@ void motor_basic(int dir) {
     default:
       break;
   }
-}    
-
-
-void boxcar_addValue(int add,int set){
-    int offset = set*MovingAverageWidth;
-    if(boxcar_index[set] == MovingAverageWidth+offset){
-        boxcar_index[set] = 0;
-        boxcar[boxcar_index[set]+offset] = add/MovingAverageWidth;
-    } else{
-        boxcar_index[set]++;
-        boxcar[boxcar_index[set]+offset] = add/MovingAverageWidth;
-    }    
 }
-        
 
-int boxcarGetAverage(int set) {
-    int mean = 0;
-    int offset = set*MovingAverageWidth;
-    for (int i =0; i < MovingAverageWidth; i++){
-        mean = mean + boxcar[i+offset];       
-    }    
-    return mean;
+
+void boxcar_addValue(int add, struct boxcar *data) {
+
+  if (data-> index == MovingAverageWidth) {
+    data-> index = 0;
+    data->vals[data-> index] = add;
+  } else {
+    data-> index++;
+    data->vals[data-> index] = add;
+  }
+}
+
+
+int boxcarGetAverage(struct boxcar *data) {
+  int mean = 0;
+  for (int i = 0; i < MovingAverageWidth; i++) {
+    mean = mean + data->vals[i];
+  }
+  return mean;
 }
 
 struct w calculateWall(int xnaught, int xone,  struct w wall) {
